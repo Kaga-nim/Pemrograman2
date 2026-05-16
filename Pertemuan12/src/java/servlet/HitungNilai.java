66b1036a0b20f628fdf93e727eab1c500fda63f3
Seftia Della <seftiadellafiisyatirrodhiah@gmail.com>
2026-05-16 19:47:34 +0700

    Pertemuan12

--- Pertemuan12/src/java/servlet/HitungNilai.java
+++ Pertemuan12/src/java/servlet/HitungNilai.java
@@ -2,166 +2,56 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "HitungNilai",
-        urlPatterns = {"/HitungNilai"})
-
+@WebServlet(name = "HitungNilai", urlPatterns = {"/HitungNilai"})
 public class HitungNilai extends HttpServlet {
 
-    @Override
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         response.setContentType("text/html;charset=UTF-8");
 
-        PrintWriter out = response.getWriter();
+        String nama  = request.getParameter("nama");
+        double tugas = Double.parseDouble(request.getParameter("tugas"));
+        double uts   = Double.parseDouble(request.getParameter("uts"));
+        double uas   = Double.parseDouble(request.getParameter("uas"));
 
-        int hadir =
-                Integer.parseInt(
-                request.getParameter("hadir"));
-
-        int pertemuan =
-                Integer.parseInt(
-                request.getParameter("pertemuan"));
-
-        double tugas =
-                Double.parseDouble(
-                request.getParameter("tugas"));
-
-        double uts =
-                Double.parseDouble(
-                request.getParameter("uts"));
-
-        double uas =
-                Double.parseDouble(
-                request.getParameter("uas"));
-
-        double presensi =
-                ((double) hadir / pertemuan) * 100;
-
-        double nilaiAkhir =
-                (presensi * 0.1) +
-                (tugas * 0.2) +
-                (uts * 0.3) +
-                (uas * 0.4);
+        double nilaiAkhir = (tugas * 0.30) + (uts * 0.35) + (uas * 0.35);
 
         String grade;
-        String status;
+        if      (nilaiAkhir >= 85) grade = "A";
+        else if (nilaiAkhir >= 70) grade = "B";
+        else if (nilaiAkhir >= 60) grade = "C";
+        else if (nilaiAkhir >= 50) grade = "D";
+        else                       grade = "E";
 
-        if (nilaiAkhir >= 80) {
-
-            grade = "A";
-            status = "Lulus";
-
-        } else if (nilaiAkhir >= 70) {
-
-            grade = "B";
-            status = "Lulus";
-
-        } else if (nilaiAkhir >= 60) {
-
-            grade = "C";
-            status = "Lulus";
-
-        } else if (nilaiAkhir >= 50) {
-
-            grade = "D";
-            status = "Tidak Lulus";
-
-        } else {
-
-            grade = "E";
-            status = "Tidak Lulus";
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html><html>");
+            out.println("<head><title>Hasil Nilai</title></head>");
+            out.println("<body>");
+            out.println("<h2>Hasil Perhitungan Nilai</h2>");
+            out.println("<table border='1'>");
+            out.println("<tr><td>Nama Mahasiswa</td><td>" + nama + "</td></tr>");
+            out.println("<tr><td>Nilai Tugas (30%)</td><td>" + tugas + "</td></tr>");
+            out.println("<tr><td>Nilai UTS (35%)</td><td>" + uts + "</td></tr>");
+            out.println("<tr><td>Nilai UAS (35%)</td><td>" + uas + "</td></tr>");
+            out.println("<tr><td><b>Nilai Akhir</b></td><td><b>" + String.format("%.2f", nilaiAkhir) + "</b></td></tr>");
+            out.println("<tr><td><b>Grade</b></td><td><b>" + grade + "</b></td></tr>");
+            out.println("</table>");
+            out.println("<br/><a href='index.html'>Kembali</a>");
+            out.println("</body></html>");
         }
-
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Menghitung Nilai</title>");
-        out.println("</head>");
-
-        out.println("<body>");
-
-        out.println("<h1>Menghitung Nilai</h1>");
-
-        out.println("<form>");
-
-        out.println("<table>");
-
-        out.println("<tr>");
-        out.println("<td>Jumlah hadir</td>");
-        out.println("<td>");
-        out.println("<input type='text' value='" + hadir + "'>");
-        out.println("</td>");
-        out.println("</tr>");
-
-        out.println("<tr>");
-        out.println("<td>Jumlah pertemuan</td>");
-        out.println("<td>");
-        out.println("<input type='text' value='" + pertemuan + "'>");
-        out.println("</td>");
-        out.println("</tr>");
-
-        out.println("<tr>");
-        out.println("<td>Nilai tugas</td>");
-        out.println("<td>");
-        out.println("<input type='text' value='" + tugas + "'>");
-        out.println("</td>");
-        out.println("</tr>");
-
-        out.println("<tr>");
-        out.println("<td>Nilai UTS</td>");
-        out.println("<td>");
-        out.println("<input type='text' value='" + uts + "'>");
-        out.println("</td>");
-        out.println("</tr>");
-
-        out.println("<tr>");
-        out.println("<td>Nilai UAS</td>");
-        out.println("<td>");
-        out.println("<input type='text' value='" + uas + "'>");
-        out.println("</td>");
-        out.println("</tr>");
-
-        out.println("<tr>");
-        out.println("<td>Nilai Akhir</td>");
-        out.println("<td>");
-        out.println("<input type='text' value='" + nilaiAkhir + "'>");
-        out.println("</td>");
-        out.println("</tr>");
-
-        out.println("<tr>");
-        out.println("<td>Grade</td>");
-        out.println("<td>");
-        out.println("<input type='text' value='" + grade + "'>");
-        out.println("</td>");
-        out.println("</tr>");
-
-        out.println("<tr>");
-        out.println("<td>Status</td>");
-        out.println("<td>");
-        out.println("<input type='text' value='" + status + "'>");
-        out.println("</td>");
-        out.println("</tr>");
-
-        out.println("<tr>");
-        out.println("<td></td>");
-        out.println("<td>");
-        out.println("<input type='button' value='Kembali' onclick='history.back()'>");
-        out.println("</td>");
-        out.println("</tr>");
-
-        out.println("</table>");
-
-        out.println("</form>");
-
-        out.println("</body>");
-        out.println("</html>");
     }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException { processRequest(request, response); }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException { processRequest(request, response); }
 }
---
+++ Pertemuan12/web/TestJSP.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Test JSP</title>
+    </head>
+    <body>
+        <h2>Ini adalah halaman TestJSP</h2>
+        <p>File ini digunakan untuk uji coba URL Pattern di web.xml</p>
+        <a href="index.html">Kembali</a>
+    </body>
+</html>
