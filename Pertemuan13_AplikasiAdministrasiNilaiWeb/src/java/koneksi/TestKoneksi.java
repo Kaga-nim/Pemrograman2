32d19b8c4ff73088b25a2cd00392801a574e8fc5
Seftia Della <seftiadellafiisyatirrodhiah@gmail.com>
2026-05-20 19:41:47 +0700

    Pertemuan 13

---
+++ Pertemuan13_AplikasiAdministrasiNilaiWeb/src/java/koneksi/TestKoneksi.java
@@ -0,0 +1,14 @@
+package koneksi;
+
+public class TestKoneksi {
+
+    public static void main(String[] args) {
+
+        KoneksiDB koneksi =
+        new KoneksiDB();
+
+        koneksi.getKoneksi();
+
+    }
+
+}
